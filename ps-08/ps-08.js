dd6a4142e77dac7ec7a53680e080e1f8bc8c33a3
chenshen0406 <chens_sogood@sina.com>
2016-11-08 14:23:20 -0500

    add 08

---
+++ ps-08/ps-08.js
@@ -0,0 +1,26 @@
+var ctx = document.getElementById("walk").getContext('2d');
+
+var beginX = 250;
+var beginY = 250;
+
+var clearScreen = function() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0,0,500,500);
+};
+var drawDot = function (x, y) {
+  clearScreen();
+  var size = 30;
+ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 50%, 50%)';
+  ctx.strokeStyle = ''
+  beginY = beginY + 31;
+  if(beginY > 500) {
+    beginY = 0;
+    beginX = beginX + 31;
+  }
+  if (beginX > 500) {
+    beginX = 0;
+  }
+  ctx.fillRect(beginX, beginY, size, size);
+};
+
+setInterval(drawDot, 100);
